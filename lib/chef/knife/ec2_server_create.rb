8e3242aeed524ad13df0b4f22fe397a7eb9871e3
Jeff Morgan <pair+jeff.morgan@samsung.com>
2014-02-11 11:58:34 -0800

    Added comments to figure out why eip is failing3

--- lib/chef/knife/ec2_server_create.rb
+++ lib/chef/knife/ec2_server_create.rb
@@ -741,6 +741,7 @@ class Chef
       end
 
       def wait_for_direct_sshd(hostname, ssh_port)
+        pp "hostname = #{hostname} port = #{ssh_port} ssh_connect_host = #{ssh_connect_host}"
         print(".") until tcp_test_ssh(ssh_connect_host, ssh_port) {
           sleep @initial_sleep_delay ||= (vpc_mode? ? 40 : 10)
           puts("done")
